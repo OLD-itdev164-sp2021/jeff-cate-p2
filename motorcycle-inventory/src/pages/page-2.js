178fbc4cb266002f32233389bfb8c3d170e30239
JeffACate <Jeff.A.Cate@gmail.com>
2021-03-07 21:28:01 -0600

    Contentful data accessed.

--- motorcycle-inventory/src/pages/page-2.js
+++ motorcycle-inventory/src/pages/page-2.js
@@ -1,28 +1,47 @@
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import * as React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const SecondPage = ({data}) => (
   <Layout>
     <SEO title="Page two" />
     <h1>This is where the magic happens!</h1>
+    <ul>
+    {
+      data.allContentfulMotorcycle.edges.map(edge => (
+        <li>
+          <Link to={edge.node.slug} key={edge.node.id}>
+            <img src={edge.node.image.fluid.src} alt='motorcycle pic' />
+          </Link> 
+        </li>
+      ))
+    }
+    </ul>
   </Layout>
 )
 
 export default SecondPage
 
 export const  query = graphql`
+query{
 	allContentfulMotorcycle{
     edges{
       node{
         slug
         year
+    		image {
+    		  id
+          fluid {
+            src
+          }
+    		}
         make
         model
         price
       }
     }
   }
+}
 `
