75b3889658f7effad58d8a1f8169b3979e9633fc
JeffACate <Jeff.A.Cate@gmail.com>
2021-03-16 23:42:27 -0500

    update

--- motorcycle-inventory/src/templates/motorcycle-post.js
+++ motorcycle-inventory/src/templates/motorcycle-post.js
@@ -4,33 +4,31 @@ import Layout from '../components/layout';
 import { navigate } from '@reach/router'
 
 const MotorcyclePost = ({ data }) => {
-    const { image, year, price, make, model, displacement, description} = data.contentfulMotorcycle;
-
+    const { image, year, price, make, model, displacement, description } = data.contentfulMotorcycle;
     return (
         <Layout>
-            <img src={image.fluid.src} style={{maxHeight:`530px`}}  alt="motorcycle"></img>
+            <img src={image.fluid.src} style={{maxHeight:`500px`}}  alt="motorcycle"></img>
             <h2> {make} {model}</h2>
             
-            <p style={{textAlign:`left`, minWidth: `340px`,maxWidth: `750px`,margin: `0 auto`}}>
+            <p style={{ textAlign:`left`, minWidth: `340px`,maxWidth: `750px`,margin: `0 auto`}}>
                 <span style={{display:`block`}}>Year: {year}</span>
-                <span style={{display:`block`}}>Price: {price}</span>
-                <span style={{display:`block`}}>displacement: {displacement} cc</span>
-                <span style={{display:`block`}}>{description.description}</span>
-                <span style={{display:`block`, marginTop:`1rem`, textAlign:`center`}}>
-                    <button 
-                        // to="/page-2/" 
-                        style={{
-                            background:`black`, 
-                            color:`rgb(255,165,0)`,
-                        }}
-                        onClick={() => {
-                            navigate('/page-2/');
-                          }}
-                    >
-                        {`<--`} back to list
-                    </button>
-                </span>
+                <span style={{display:`block`}}>Price: ${price}</span>
+                <span style={{display:`block`}}>Displacement: {displacement} cc</span>
+                <span style={{display:`block`}}>Description: </span>
             </p>
+            <div dangerouslySetInnerHTML={{__html: description.childrenMarkdownRemark.html}}></div>
+            <button 
+                style={{
+                    background:`black`, 
+                    color:`rgb(255,165,0)`,
+                    marginTop:`1rem`
+                }}
+                onClick={() => {
+                    navigate('/page-2/');
+                    }}
+            >
+                {`<--`} back to list
+            </button>
 
         </Layout>
     );
@@ -41,19 +39,21 @@ export default MotorcyclePost;
 export const pageQuery = graphql`
 query motorcycleQuery($slug: String!) {
     contentfulMotorcycle(slug: {eq: $slug}) {
-        image{
-            fluid {
-                src
-            }
+      year
+      make
+      model
+      price
+      displacement
+      image {
+        fluid {
+          src
         }
-        year
-        make
-        price
-        model
-        displacement
-        description {
-            description
+      }
+      description {
+        childrenMarkdownRemark {
+          html
         }
+      }
     }
   }
 `
