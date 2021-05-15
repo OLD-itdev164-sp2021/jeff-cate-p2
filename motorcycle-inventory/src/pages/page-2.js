03d2ac142b321d6a9e0be15b0864cbc2bdf0f9e9
JeffACate <Jeff.A.Cate@gmail.com>
2021-05-15 17:08:32 -0500

    implement list component in page-2 | the motorcycle list.

--- motorcycle-inventory/src/pages/page-2.js
+++ motorcycle-inventory/src/pages/page-2.js
@@ -1,25 +1,26 @@
-import { graphql, Link } from "gatsby"
 import * as React from "react"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { List, ListItem } from "../components/List"
 
 const SecondPage = ({data}) => (
   <Layout>
     <SEO title="Page two" />
     <h1>Current inventory</h1>
-    <ul>
+    <List width={[1, 1/2, 2/3]} p={2}>
     {
       data.allContentfulMotorcycle.edges.map(edge => (
-        <li style={{listStyle:`none`}} key={edge.node.id}>
+        <ListItem style={{listStyle:`none`}} key={edge.node.id}>
           <Link to={`/${edge.node.slug}`} key={edge.node.id} >
             <img src={edge.node.image.fluid.src} height='200px' alt='motorcycle pic' />
             <p>{edge.node.year} {edge.node.make} {edge.node.model}</p>
           </Link> 
-        </li>
+        </ListItem>
       ))
     }
-    </ul>
+    </List>
   </Layout>
 )
 
@@ -35,7 +36,7 @@ query{
         year
     		image {
     		  id
-          fluid {
+          fluid(maxWidth: 600) {
             src
           }
     		}
